b2adcc160928b87e9b12331d65db81c19a5edfa9
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-29 21:28:46 +0000

    Add Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,15 +1,9 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
-
-// export const getLogs = () => {
-//   return async (dispatch) => {
-//     setLoading();
-
-//     const res = await fetch('/logs');
-//     const data = await res.json();
-
-//     dispatch({ type: GET_LOGS, payload: data })
-//   }
-// }
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG
+} from './types';
 
 // Get logs from server
 export const getLogs = () => async dispatch => {
@@ -20,6 +14,27 @@ export const getLogs = () => async dispatch => {
     const data = await res.json();
 
     dispatch({ type: GET_LOGS, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+}
+
+// Add new log
+export const addLog = (log) => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({ type: ADD_LOG, payload: data });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
